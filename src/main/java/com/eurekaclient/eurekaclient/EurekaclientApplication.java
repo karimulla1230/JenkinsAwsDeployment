2c1662718c77b8853eeba8639f870e8c26b77587
Karimulla Shaik <karimulla.shaik@otsi-usa.com>
2021-06-09 13:03:00 +0530

    modified

--- src/main/java/com/eurekaclient/eurekaclient/EurekaclientApplication.java
+++ src/main/java/com/eurekaclient/eurekaclient/EurekaclientApplication.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @SpringBootApplication
-@EnableEurekaClient
 public class EurekaclientApplication {
 
 	public static void main(String[] args) {
